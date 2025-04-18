0b0ec0c0cc8a3154f22014f3bcf99b73530ea1fb
Jbxsa01 <bjane.asmaa1@gmail.com>
2025-04-18 10:49:36 +0100

    Initial commit

---
+++ src/main/java/ma/emsi/jeecontrole/JeeControleApplication.java
@@ -0,0 +1,13 @@
+package ma.emsi.jeecontrole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeeControleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JeeControleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/emsi/jeecontrole/JeeControleApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.emsi.jeecontrole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeeControleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
